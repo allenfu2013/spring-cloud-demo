3466084304a403c1c442c37f7054101074e35b93
fuyong <fuyong@niwodai.net>
2016-10-26 11:12:43 +0800

    add access control to api-gateway

--- api-gateway/src/main/java/org/allen/springcloud/Application.java
+++ api-gateway/src/main/java/org/allen/springcloud/Application.java
@@ -1,12 +1,19 @@
 package org.allen.springcloud;
 
+import org.allen.springcloud.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class Application {
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
---
+++ api-gateway/src/main/java/org/allen/springcloud/filter/AccessFilter.java
@@ -0,0 +1,48 @@
+package org.allen.springcloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        // pre：可以在请求被路由之前调用
+        // routing：在路由请求时候被调用
+        // post：在routing和error过滤器之后被调用
+        // error：处理请求时发生错误时被调用
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("accessToken");
+        if(accessToken == null) {
+            logger.warn("access token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        logger.info("access token ok");
+        return null;
+    }
+}
